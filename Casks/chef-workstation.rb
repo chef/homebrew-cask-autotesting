641f53074dc0716bd4a8375f1661b80a15feb21e
Chef Expeditor <expeditor@chef.io>
2020-05-19 11:40:53 -0700

    Bump chef-workstation to 0.18.3
    After making all changes to the cask:
    
    - [x] `brew cask audit --download Casks/chef-workstation.rb` is error-free.
    - [x] `brew cask style --fix Casks/chef-workstation.rb` reports no offenses.
    - [x] The commit message includes the cask’s name and version.
    - [x] The submission is for stable version.

--- Casks/chef-workstation.rb
+++ Casks/chef-workstation.rb
@@ -1,6 +1,6 @@
 cask 'chef-workstation' do
-  version '0.17.5'
-  sha256 'invalid, testing'
+  version '0.18.3'
+  sha256 '53312b80cfd0bbc29b3be73cfc0a6c438fd1873c60c0752ac3b636c62fdcbab3'
 
   url "https://packages.chef.io/files/stable/chef-workstation/#{version}/mac_os_x/10.13/chef-workstation-#{version}-1.dmg"
   appcast 'https://omnitruck.chef.io/stable/chef-workstation/metadata?p=mac_os_x&pv=10.14&m=x86_64&v=latest'
